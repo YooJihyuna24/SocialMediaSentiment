3947b33cf7b76a82fd8ed1e50b74dff49045af63
Robbie Sumner <robbiecsumner@gmail.com>
2025-03-21 14:44:09 +0100

    format and clean up

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import streamlit as st
-import dotenv
-import os
 
-pg = st.navigation([
-    st.Page("subreddit_dashboard.py", title="Subreddit Dashboard"),
-    st.Page("posts_dashboard.py", title="Posts Dashboard"),
-])
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+
+pg = st.navigation(
+    [
+        st.Page("subreddit_dashboard.py", title="Subreddit Dashboard"),
+        st.Page("posts_dashboard.py", title="Posts Dashboard"),
+    ]
+)
 pg.run()
