6840fbf5c4a14091d6da1cea284f00cf5aa9d560
YooJihyuna24 <jihyun.yoo@gmx.de>
2025-04-23 16:07:52 +0200

    fix: correct spelling of 'multilingual' in models dictionary

--- src/models.py
+++ src/models.py
@@ -2,6 +2,6 @@ models = {
     "distilbert": "bhadresh-savani/distilbert-base-uncased-emotion",
     "distilroberta": "j-hartmann/emotion-english-distilroberta-base",
     "roberta": "cardiffnlp/twitter-roberta-base-sentiment-latest",
-    "multilangual": "tabularisai/multilingual-sentiment-analysis",
+    "multilingual": "tabularisai/multilingual-sentiment-analysis",
     "political": "m-newhauser/distilbert-political-tweets",
 }
